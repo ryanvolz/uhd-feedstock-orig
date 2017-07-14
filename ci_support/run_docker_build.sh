43ef30cad6856056063d6f2406a8c822f5be6bbb
Ryan Volz <rvolz@mit.edu>
2017-07-14 17:08:35 -0400

    Fix build with corrected license_file path, re-render.

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -58,7 +58,7 @@ conda clean --lock
 conda install --yes --quiet conda-forge-build-setup
 source run_conda_forge_build_setup
 
-# Embarking on 2 case(s).
+# Embarking on 3 case(s).
     set -x
     export CONDA_PY=27
     set +x
@@ -67,6 +67,12 @@ source run_conda_forge_build_setup
 
     set -x
     export CONDA_PY=35
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    upload_or_check_non_existence /recipe_root ryanvolz --channel=main || exit 1
+
+    set -x
+    export CONDA_PY=36
     set +x
     conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root ryanvolz --channel=main || exit 1

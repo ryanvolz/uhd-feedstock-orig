3d3bf8712fb2ecee7b2e7b24fea1549ba9d1f123
Ryan Volz <rvolz@mit.edu>
2017-07-19 14:06:23 -0400

    Disable doxygen/manual and add conda tests.

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,11 +6,35 @@ CXXFLAGS="${CXXFLAGS} -D_GLIBCXX_USE_CXX11_ABI=0"
 
 mkdir build
 cd build
+# enable gnuradio components explicitly so we get build error when unsatisfied
+# the following are disabled:
+#   DOXYGEN/MANUAL because we don't need docs in the conda package
+#   E100/E300 are for embedded devices and are disable by default
+#   GPSD needs gpsd
 cmake ../ \
     -DBOOST_ROOT=$PREFIX \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DLIB_SUFFIX=""
+    -DLIB_SUFFIX="" \
+    -DENABLE_B100=ON \
+    -DENABLE_B200=ON \
+    -DENABLE_C_API=ON \
+    -DENABLE_DOXYGEN=OFF \
+    -DENABLE_E100=OFF \
+    -DENABLE_E300=OFF \
+    -DENABLE_EXAMPLES=ON \
+    -DENABLE_GPSD=OFF \
+    -DENABLE_LIBUHD=ON \
+    -DENABLE_MAN_PAGES=ON \
+    -DENABLE_MANUAL=OFF \
+    -DENABLE_OCTOCLOCK=ON \
+    -DENABLE_N230=ON \
+    -DENABLE_TESTS=ON \
+    -DENABLE_UTILS=ON \
+    -DENABLE_USB=ON \
+    -DENABLE_USRP1=ON \
+    -DENABLE_USRP2=ON \
+    -DENABLE_X300=ON \
 make -j${CPU_COUNT}
 make test
 make install

18c0fe5779599392aa7ce89e2a639fb8cafcc4fe
Ryan Volz <rvolz@mit.edu>
2018-04-11 17:03:36 -0400

    3.11.0.1

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ source activate "${CONDA_DEFAULT_ENV}"
 # make builds with gcc>=5 compatible with conda-forge, currently using gcc<5
 CXXFLAGS="${CXXFLAGS} -D_GLIBCXX_USE_CXX11_ABI=0"
 
+cd host
 mkdir build
 cd build
 # enable gnuradio components explicitly so we get build error when unsatisfied
